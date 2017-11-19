6b2245872066b84f02345dde3e4a4459165acd04
shkhan786 <shkhan786haider@gmail.com>
2017-11-19 18:11:12 +0530

    added class

---
+++ JMTestLib/Classes/test/TestHello.swift
@@ -0,0 +1,13 @@
+//
+//  TestHello.swift
+//  JMTestLib
+//
+//  Created by S Haider Khan on 19/11/17.
+//
+
+import Foundation
+public class TestHello: NSObject{
+    public static func hello(){
+        print("Hello this is my first cocoapod lib.")
+    }
+}
